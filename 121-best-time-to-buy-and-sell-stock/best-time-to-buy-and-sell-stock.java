db3853c1567159c691297b1f5b8a7be67e9813ca
SatvikaKoneti <ifykykyk06@gmail.com>
2026-08-21 04:53:54 +0530

    Time: 2 ms (43.46%) | Memory: 94.3 MB (86.00%) - LeetSync

--- 121-best-time-to-buy-and-sell-stock/best-time-to-buy-and-sell-stock.java
+++ 121-best-time-to-buy-and-sell-stock/best-time-to-buy-and-sell-stock.java
@@ -4,7 +4,7 @@ class Solution {
         int min = prices[0] ;
         int max = 0 ;
 
-        for(int i =  0 ; i < prices.length ; i++)
+        for(int i =  1 ; i < prices.length ; i++)
         {
             if(prices[i]<min)
             {
